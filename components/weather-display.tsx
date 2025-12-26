a6ff94a63caf9d7aee49f5ccd9921d80d5132f30
Rodrigo <rodrigo_ward@hotmail.com>
2025-12-26 04:44:13 -0300

    fix: removendo console.log

--- components/weather-display.tsx
+++ components/weather-display.tsx
@@ -44,8 +44,6 @@ export function WeatherDisplay({ weather, language }: WeatherDisplayProps) {
 
   const formatTemp = (temp: number) => Math.round(temp)
 
-  console.log(weather);
-
   return (
     <div className="space-y-6 animate-in fade-in slide-in-from-bottom-4 duration-500">
       {/* Card Principal */}
